070ccf52642e7db30e5ddcffef068a437997007c
Hung Quoc <nguyenhungquoc1410@gmail.com>
2022-09-23 18:40:46 +0700

    set up firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getDatabase, ref } from 'firebase/database'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyByckPKS9VLEtAo9PCpCm_MhxnChcznTjo',
+  authDomain: 'nathan-boardgame.firebaseapp.com',
+  projectId: 'nathan-boardgame',
+  storageBucket: 'nathan-boardgame.appspot.com',
+  messagingSenderId: '685742545433',
+  appId: '1:685742545433:web:f553f7916533124fcca888',
+  measurementId: 'G-B87DNLZV6V',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// Initialize Firebase Authentication and get a reference to the service
+export const Auth = getAuth(app)
+
+// Initialize Realtime Database and get a reference to the service
+export const Database = getDatabase(app)
+export const DatabaseRef = ref(Database)
