2638b1bd50588673966779b227aa7d7959de04bc
Hung Quoc <nguyenhungquoc1410@gmail.com>
2022-09-30 18:13:57 +0700

    add show name on hover to avatar

--- src/routes/room/components/player-avatar.tsx
+++ src/routes/room/components/player-avatar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Avatar, Badge, useTheme } from '@mui/material'
+import { Avatar, Badge, Tooltip, useTheme } from '@mui/material'
 
 import { IPlayer } from '../../../services/firebase'
 
@@ -13,18 +13,20 @@ const PlayerAvatar: React.FC<PlayerAvatarProps> = ({ data }) => {
 
   return (
     <div className='flex flex-1 justify-center items-center'>
-      <Badge badgeContent={data.points} color='primary'>
-        <Avatar
-          sx={{
-            width: 56,
-            height: 56,
-            backgroundColor: data.color,
-            color: theme.palette.getContrastText(data.color),
-          }}
-        >
-          {data.name[0]}
-        </Avatar>
-      </Badge>
+      <Tooltip title={data.name} placement='top'>
+        <Badge badgeContent={data.points} color='primary'>
+          <Avatar
+            sx={{
+              width: 56,
+              height: 56,
+              backgroundColor: data.color,
+              color: theme.palette.getContrastText(data.color),
+            }}
+          >
+            {data.name[0]}
+          </Avatar>
+        </Badge>
+      </Tooltip>
     </div>
   )
 }
